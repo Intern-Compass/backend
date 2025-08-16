239b96eef822ae00429e745b0d8efc84f3e004ac
Adedara Adeloro <adedara.adeloro@mtn.com>
2025-08-17 00:08:26 +0100

    initial commit

---
+++ src/routers/intern_auth_router.py
@@ -0,0 +1,9 @@
+"""
+This module would handle any thing regarding auth for an intern.
+This includes:
+    - Login
+    - Logout
+    - Registration
+    - Forgot Password (Request)
+    - Reset Password
+"""
