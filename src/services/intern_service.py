14238af36a09f8d25c0f63dea8124e4ee3b5091f
Adedara Adeloro <adedara.adeloro@mtn.com>
2025-08-17 00:11:59 +0100

    Add new comments

--- src/services/intern_service.py
+++ src/services/intern_service.py
@@ -3,4 +3,5 @@ This module will handle:
     - Creation of accounts for intern
     - Viewing supervisor intern was assigned to
     - Viewing tasks assigned by the supervisor
+    Anything else the intern_auth router might need
 """
