52e1387daa68ad920c4e2620298b4b8127b7fd16
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>
2025-09-13 17:29:22 +0100

    feat: override token base methods to get proper typing on encoding and decoding
    Signed-off-by: Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>

--- src/infra/token.py
+++ src/infra/token.py
@@ -5,6 +5,7 @@ from uuid import UUID, uuid4
 
 from jwt import PyJWTError, decode, encode
 
+from ..schemas import UserOutModel
 from ..settings import settings
 
 
@@ -30,11 +31,12 @@ TOKEN_LIFETIMES: dict[TokenType, timedelta] = {
 class TokenBase(ABC):
     token_type: TokenType
 
-    def new(self, sub: str | UUID) -> str:
+    def new(self, sub: str | UUID, data: dict | None = None) -> str:
         if isinstance(sub, UUID):
             sub = str(sub)
         return encode(
             payload={
+                "data": data,
                 "exp": self.token_type.lifetime.total_seconds(),
                 "iat": datetime.now(UTC),
                 "jti": uuid4(),
@@ -45,7 +47,8 @@ class TokenBase(ABC):
             algorithm=settings.ALGO,
         )
 
-    def decode(self, token: str) -> str | UUID:
+    @staticmethod
+    def decode(token: str) -> dict:
         try:
             claims: dict = decode(
                 token, key=settings.SECRET_KEY, algorithms=[settings.ALGO]
@@ -62,6 +65,27 @@ class TokenBase(ABC):
 class AccessToken(TokenBase):
     token_type = TokenType.ACCESS
 
+    # noinspection PyMethodOverriding
+    def new(self, user: UserOutModel) -> str:
+        user_id = user.user_id
+        return super().new(sub=user_id, data=user.model_dump())
+
+    @staticmethod
+    def decode(token: str) -> UserOutModel:
+        claims = super().decode(token)
+        data = claims["data"]
+        return UserOutModel.model_validate(data)
+
 
 class PasswordResetToken(TokenBase):
     token_type = TokenType.PASSWORD_RESET
+
+    # noinspection PyMethodOverriding
+    def new(self, user_id: UUID):
+        return super().new(sub=user_id)
+
+    @staticmethod
+    def decode(token: str) -> UUID:
+        claims = super().decode(token)
+        user_id = claims["sub"]
+        return user_id
