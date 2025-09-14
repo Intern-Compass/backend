0497616723527328073d7a33b0e92537b97e7971
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>
2025-09-14 22:13:21 +0100

    feat: adapt `intern_router.py` to use the adjusted `get_intern_user` dependency
    Signed-off-by: Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>

--- src/routers/intern_router.py
+++ src/routers/intern_router.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from fastapi.responses import ORJSONResponse
 
+from ..schemas import UserOutModel
 from ..schemas.intern_schemas import ISupervisor
 from ..schemas.project_schemas import ProjectOutModel
 from ..schemas.task_schemas import TaskOutModel
@@ -20,10 +21,10 @@ router: APIRouter = APIRouter(prefix="/intern", tags=["Intern"])
 
 @router.get("/supervisor", tags=["Dashboard"])
 async def get_intern_supervisor(
-    user: Annotated[dict, Depends(get_intern_user)],
-    intern_service: Annotated[InternService, Depends()]
+    user: Annotated[UserOutModel, Depends(get_intern_user)],
+    intern_service: Annotated[InternService, Depends()],
 ) -> ISupervisor:
-    supervisor = await intern_service.get_supervisor_by_intern_user_id(user.get("sub"))
+    supervisor = await intern_service.get_supervisor_by_intern_user_id(user.user_id)
     if supervisor:
         return supervisor
     return ORJSONResponse({"message": "Supervisor not found"}, status_code=404)
@@ -31,29 +32,29 @@ async def get_intern_supervisor(
 
 @router.get("/task", tags=["Tasks", "Dashboard"])
 async def get_intern_tasks(
-    user: Annotated[dict, Depends(get_intern_user)],
-    intern_service: Annotated[InternService, Depends()]
+    user: Annotated[UserOutModel, Depends(get_intern_user)],
+    intern_service: Annotated[InternService, Depends()],
 ) -> list[TaskOutModel]:
-    return await intern_service.get_tasks(user.get("sub"))
+    return await intern_service.get_tasks(user.user_id)
 
 
 @router.get("/projects", tags=["Projects", "Dashboard"])
 async def get_projects(
-    user: Annotated[dict, Depends(get_intern_user)],
-    intern_service: Annotated[InternService, Depends()]
+    user: Annotated[UserOutModel, Depends(get_intern_user)],
+    intern_service: Annotated[InternService, Depends()],
 ) -> list[ProjectOutModel]:
-    return await intern_service.get_projects(user.get("sub"))
+    return await intern_service.get_projects(user.user_id)
 
 
 @router.get("/todos", tags=["Todos", "Dashboard"])
 async def get_intern_todos(
-    user: Annotated[dict, Depends(get_intern_user)],
+    user: Annotated[UserOutModel, Depends(get_intern_user)],
     todo_service: Annotated[TodoService, Depends()],
     limit: int | None = None,
     offset: int | None = None,
     done: bool | None = None,
 ) -> list[TodoOutModel]:
-    todos = await todo_service.get_todo(user.get("sub"), done, limit, offset)
+    todos = await todo_service.get_todo(user.user_id, done, limit, offset)
     return todos
 
 
