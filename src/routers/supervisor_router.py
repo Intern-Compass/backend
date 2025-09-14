59b2af317584b5cf92f3a5d1ce155d85c7f5fb08
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>
2025-09-14 22:52:03 +0100

    feat: adapt the usages of the `get_supervisor_user`
    Signed-off-by: Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>

--- src/routers/supervisor_router.py
+++ src/routers/supervisor_router.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter
 from fastapi.params import Depends
 
+from src.schemas.supervisor_schemas import SupervisorOutModel
 from src.services.matching_service import MatchingService
 from src.services.supervisor_service import SupervisorService
 from src.utils import get_supervisor_user
@@ -15,17 +16,14 @@ router: APIRouter = APIRouter(prefix="/supervisor", tags=["Supervisor"])
 @router.get("/my-interns")
 async def get_interns(
     supervisor_service: Annotated[SupervisorService, Depends()],
-    supervisor: Annotated[dict, Depends(get_supervisor_user)],
+    supervisor: Annotated[SupervisorOutModel, Depends(get_supervisor_user)],
 ):
-    return await supervisor_service.get_interns(
-        supervisor_id=supervisor.get("supervisor_id")
-    )
+    return await supervisor_service.get_interns(supervisor_id=supervisor.supervisor_id)
 
 
-@router.post("/perform-matching")
+@router.post("/perform-matching", dependencies=[Depends(get_supervisor_user)])
 async def perform_matches(
     matching_service: Annotated[MatchingService, Depends()],
-    _: Annotated[dict, Depends(get_supervisor_user)],
 ):
     # Will refactor this to its own dedicated router.
     return await matching_service.perform_bulk_matching()
